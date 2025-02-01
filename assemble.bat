d1d9ba4b871930cbab17063c43ffb23a22ea37b8
MDTravis <59547361+MDTravisYT@users.noreply.github.com>
2025-02-02 07:02:09 +0900

    switch assemblers

--- assemble.bat
+++ assemble.bat
@@ -1,6 +1,7 @@
-@echo off
-if not exist OUT mkdir OUT
-"TOOL/vasmm68k_psi-x.exe" -altlocal -m68000 -maxerrors=0 -Fbin -start=0 -o "OUT/MD0225.BIN" -L "OUT/MD0225.LST" -Lall "SRC/MAIN.S" 2> _errors.log
-type _errors.log
-del _errors.log
-pause
+echo %date% %time% > time.txt
+
+IF NOT EXIST OUT MKDIR OUT
+asm68k /k /m /o ws+ /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /o ae- /o v+ /o c+ /p /e __DEBUG__=1 SRC/MAIN.S, OUT/MD0225.BIN >OUT/MD0225.LOG, OUT/MD0225.SYM, OUT/MD0225.LST
+convsym OUT/MD0225.SYM OUT/MD0225.BIN -a
+
+del time.txt
